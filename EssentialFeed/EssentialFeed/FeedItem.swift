a204985c833eecb63e4e7ecab134a79a3564dac8
Pawel Klapuch <pawelklapuch@gmail.com>
2022-12-25 18:03:38 +0100

    Source Code.

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Pawel Klapuch on 12/25/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
